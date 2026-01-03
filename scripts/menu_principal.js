03b62131f4f6967212912b67128805f82a47beb5
WillyStrange <WillyStrange@gmail.com>
2026-01-03 12:53:53 +0100

    other changes

--- scripts/menu_principal.js
+++ scripts/menu_principal.js
@@ -667,7 +667,7 @@ App.localization = {
 App.loadLocalization = function() {
     const userData = getUserData();
     const lang = userData.language || 'fr';
-    fetch(`/api/data/${lang}`)
+    fetch(`/api/data/localization/${lang}`)
         .then(res => res.json())
         .then(data => {
             App.localization = data;
